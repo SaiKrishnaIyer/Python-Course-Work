980b74de48202f3f1c54bb6e3dac549c573ea443
SaiKrishnaIyer <ksai85075@gmail.com>
2025-12-13 12:56:12 +0530

    Added Math Functions

--- Classwork/21.system_modules.py
+++ Classwork/21.system_modules.py
@@ -21,7 +21,7 @@ print(platform.processor())
 
 import random
 
-random.seed(10) # Same Output after Excecution
+#random.seed(10) # Same Output after Excecution
 print(random.random())
 print(random.randint(1, 10))
 print(random.uniform(1, 10))
@@ -32,3 +32,34 @@ print(random.choices(names, k = 3))
 print("Before: ", names)
 random.shuffle(names)
 print("after: ", names)
+
+import math
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(16))
+print(math.pow(2, 5))
+print(math.ceil(12.3))
+print(math.ceil(12.0000001))
+print(math.ceil(12.23))
+print(math.ceil(12.83))
+
+print(math.floor(12.9999999999))
+print(math.floor(12.3))
+print(math.floor(12.83))
+print(math.floor(12.9))
+
+print(math.fabs(-12.3))
+
+print(math.factorial(7))
+
+print(math.gcd(50, 100))
+
+print(math.sin(30))
+print(math.cos(30))
+print(math.tan(30))
+
+print(math.log(2, 2))
+
+print(math.degrees(30))
+print(math.radians(12))
